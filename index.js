4040d900b2c170f2a3430210ac7ffdea3baf4710
Frank Schoonenberg <fpcschoonenberg@hotmail.com>
2021-03-24 21:16:00 +0100

    array filter and map

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
-* const ikRockArrowFunctions = () => "Joe, ik rock de arrow functions";
-* console.log(`resultaat arrowrockfunction ${ikRockArrowFunctions()}`);
+const ikRockArrowFunctions = () => "Joe, ik rock de arrow functions";
+console.log(`resultaat arrowrockfunction ${ikRockArrowFunctions()}`);
 
 const fivePlusSeven = () => 5 + 7;
 console.log(`resultaat fivePlusSeven  ${fivePlusSeven()}`);
@@ -13,28 +13,31 @@ const createObject = () => ({ greeting: "hoi" });
 console.log(`resultaat createObject  ${createObject()}`);
 
 let fruits = [
-    { name: "banana", cost: 10 },
-    { name: "orange", cost: 15 },
-    { name: "kiwi", cost:25 },
-    { name: "apple", cost: 15 },
-    { name: "coconut", cost: 15 },
-    { name: "kaki", cost: 5 },
-    { name: "cherrry", cost: 1 },
-    { name: "mango", cost: 2 },
-    { name: "peach", cost: 13 }
+    { name: "banana", cost: 10, country: "brasil"},
+    { name: "orange", cost: 15, country: "brasil"},
+    { name: "orange", cost: 15, country: "congo"},
+    { name: "kiwi", cost: 25 ,country: "brasil"},
+    { name: "apple", cost: 15 ,country: "brasil"},
+    { name: "coconut", cost: 15, country: "cuba" },
+    { name: "kaki", cost: 5, country: "brasil" },
+    { name: "cherrry", cost: 1, country: "brasil"},
+    { name: "mango", cost: 2, country: "brasil" },
+    { name: "peach", cost: 13, country: "brasil"}
 ];
 
 const selectedFruits = fruits.filter((item) => {
     
-    if (item.cost <= 5) {
+    if (item.country !== "brasil") {
         return item
     }
 });
 console.log(selectedFruits.length);
 let test = selectedFruits.length
 console.log(test);
-selectedFruits.forEach(fruit => console.log(fruit.name));
-    
+selectedFruits.forEach(fruit => console.log(`${ fruit.name } comes from ${fruit.country}`));
+   
+const fruitExtra = fruits.map(item => item.cost + 100);
+fruitExtra.forEach(item => console.log(item));
 
 
     
