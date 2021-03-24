b2692a5347171e31fe47e05e4a067e21c74f6cef
Frank Schoonenberg <fpcschoonenberg@hotmail.com>
2021-03-24 11:38:05 +0100

    excercise arrow functions

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+const ikRockArrowFunctions = () => "Joe, ik rock de arrow functions";
+console.log(`resultaat arrowrockfunction ${ikRockArrowFunctions()}`);
+
+const fivePlusSeven = () => 5 + 7;
+console.log(`resultaat fivePlusSeven  ${fivePlusSeven()}`);
+
+const sum = a => a + 5;
+console.log(`resultaat sum(100)  ${sum(100)}`);
+
+const createObject = () => ({ greeting: "hoi" });
+console.log(`resultaat createObject  ${createObject()}`);
+
