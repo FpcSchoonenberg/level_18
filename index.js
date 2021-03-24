66aff3f6fc0b2728095c1f1859c8f4ff0929116e
Frank Schoonenberg <fpcschoonenberg@hotmail.com>
2021-03-24 20:43:37 +0100

    final check

--- index.js
+++ index.js
@@ -12,3 +12,34 @@ console.log(`resultaat sum(100)  ${sum(100)}`);
 const createObject = () => ({ greeting: "hoi" });
 console.log(`resultaat createObject  ${createObject()}`);
 
+let fruits = [
+    { name: "banana", cost: 10 },
+    { name: "orange", cost: 15 },
+    { name: "kiwi", cost:25 },
+    { name: "apple", cost: 15 },
+    { name: "coconut", cost: 15 },
+    { name: "kaki", cost: 5 },
+    { name: "cherrry", cost: 1 },
+    { name: "mango", cost: 2 },
+    { name: "peach", cost: 13 }
+];
+
+const selectedFruits = fruits.filter((item) => {
+    
+    if (item.cost <= 5) {
+        return item
+    }
+});
+console.log(selectedFruits.length);
+let test = selectedFruits.length
+console.log(test);
+selectedFruits.forEach(fruit => console.log(fruit.name));
+    
+
+
+    
+
+
+
+
+
